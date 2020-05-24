5c878bbf54ec3d75f9f0daa08ce98a97abd71af5
Musppelheim <neloliver@hotmail.com>
2020-05-24 22:11:00 +0700

    equalTo added

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class MobileBankApiTestV1 {
@@ -20,6 +21,6 @@ public class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200)
-        .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+        .body("data",equalTo("some data"));
     }
 }
